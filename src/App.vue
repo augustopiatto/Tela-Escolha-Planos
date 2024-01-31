1c9e056c0c77f302f195a517cb302268e39a6607
augustopiatto <ampiatto@hotmail.com>
2024-01-31 10:15:06 -0300

    feat(plan-card): estruturacao inicial da divisao de componentes

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <script setup lang="ts">
 import { plans } from './apimock/apimock';
+import PlanCard from './components/PlanCard.vue'
 
 </script>
 
 <template>
   <div class="app">
     <div v-for="plan in plans">
-      {{plan}}
+      <PlanCard :plan="plan" :key="plan.id" />
     </div>
   </div>
 </template>
 
 <style scoped>
 .app {
-  height: 100vh;
-  width: 100vw;
+  padding: 64px 0;
+  margin: 0 auto;
 }
 </style>
---
+++ src/components/PlanCard.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { PlansAPIType } from '../helpers/types/types';
+
+defineProps<{ plan: PlansAPIType }>()
+
+</script>
+
+<template>
+  {{ plan }}
+</template>
+
+<style scoped>
+</style>
